95886a34005dfe4963bb3df7fce76d08aebd7be8
Sahil Gaba <sachingaba10@gmail.com>
2023-09-21 20:10:07 -0400

    new file added

---
+++ calculation.java
@@ -0,0 +1,26 @@
+public class calculation {
+    public static void main(String[] args) {
+        //addition
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1 + num2;
+        System.out.println(sum);
+        //division
+        int div = num1/num2;
+        System.out.println(div);
+        //substraction
+        int sub = num1-num2;
+        System.out.println(sub);
+        //multiply
+        int multi = num1*num2;
+        System.out.println(multi);
+        double wage1 = 58.60;
+        double wage2 = 20.40;
+        double wage3 = 30.05;
+        double total = wage1+wage2+wage3;
+        System.out.println(total);
+        double tax = wage1/wage2;
+        System.out.println(tax);
+
+    }
+}
